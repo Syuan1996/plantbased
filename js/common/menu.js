d112c9780c08a459b4476617300becf7e58aa6f2
Syuan1996 <js7912288@gmail.com>
2025-06-11 22:52:22 +0800

    搜尋彈窗修改

--- js/common/menu.js
+++ js/common/menu.js
@@ -74,12 +74,69 @@ let CloseMagnifier = document.querySelector('.closePopBtn');
 OpenMagnifier.addEventListener("click", (e) => {
   e.preventDefault();
   popup.classList.add("popTriger");
-})
+  startTyping();
+});
 
 CloseMagnifier.addEventListener("click", () => {
   popup.classList.remove("popTriger");
+});
+
+// 點擊灰黑色背景也能讓搜尋彈窗關掉 start
+popup.addEventListener("click", (e) => {
+  if (e.target === popup) {
+    popup.classList.remove("popTriger");
+  }
 })
 
+// 點擊灰黑色背景也能讓搜尋彈窗關掉 end
+
+// 搜尋框裡面文字打字機效果start
+let searchInput = document.querySelector('.searchBox input');
+let text = "請輸入搜尋內容....";
+let typingTimer = null; // 儲存打字機interval
+let cursorTimer = null; // 儲存游標閃爍interval
+
+function startTyping() {
+  clearInterval(typingTimer);//清除舊的打字
+  clearInterval(cursorTimer);//清除舊的閃爍
+  let i = 0;
+
+  typingTimer = setInterval(() => {
+    searchInput.placeholder = text.substring(0, i) + "│";
+    i++;
+
+    if (i > text.length) {
+      clearInterval(typingTimer);
+      endTypingAndFocus(); //結束虛擬打字游標與focus回去
+    }
+  }, 130);
+}
+
+function endTypingAndFocus() {
+
+  let count = 3;
+  let timer = setInterval(() => {
+    searchInput.placeholder = text + (count % 2 === 0 ? "│" : " ");
+    count--;
+
+    if (count <= 0) {
+      clearInterval(timer);
+      searchInput.placeholder = text;
+      searchInput.focus();
+    }
+  }, 350);
+}
+
+// 記得startTyping()放回OpenMagnifier.addEventListener上面的點擊時
+OpenMagnifier.addEventListener("click", (e) => {
+  e.preventDefault();
+  searchInput.value = "";
+  startTyping();
+
+});
+
+
+// 搜尋框裡面文字打字機效果end
 
 // 搜尋彈窗end
 
